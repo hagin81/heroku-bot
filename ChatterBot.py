6bae66209e26572f84541b07478d2a0c1ed96ab3
hagin81 <hagin81@gmail.com>
2018-03-01 21:37:41 -0600

    edited heroku

--- ChatterBot.py
+++ ChatterBot.py
@@ -4,10 +4,11 @@ import json
 import time
 
 # Twitter API Keys
-consumer_key = "Ed4RNulN1lp7AbOooHa9STCoU"
-consumer_secret = "P7cUJlmJZq0VaCY0Jg7COliwQqzK0qYEyUF9Y0idx4ujb3ZlW5"
-access_token = "839621358724198402-dzdOsx2WWHrSuBwyNUiqSEnTivHozAZ"
-access_token_secret = "dCZ80uNRbFDjxdU2EckmNiSckdoATach6Q8zb7YYYE5ER"
+# Twitter API Keys
+consumer_key = "eGQAOPryyLAJz3pJyJmlfnWL6"
+consumer_secret = "2pqISGLwJ9Apev91mvqxw84jQxOYZSu80BQkG946gA5U9mQgvn"
+access_token = "967430125376729088-JEeYufp3G5ZvrBrR6IZuJvtqs9aGFjO"
+access_token_secret = "TT6Mp19plml6ErqdrqTQZ3ihRqPtWzM3WZh41lffFs8Bs"# Setup Tweepy API Authentication
 
 # Setup Tweepy API Authentication
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
@@ -16,4 +17,26 @@ api = tweepy.API(auth, parser=tweepy.parsers.JSONParser())
 
 
 # Create a function that tweets
-# CODE GOES HERE
+def TweetOut(tweet_number):
+    tweet_text = "Can't stop. Won't stop. Chatting! This is Tweet #{}!".format(tweet_number)
+    try:
+        api.update_status(tweet_text)
+    except Exception as e:
+        print(e)
+        print("Attempted to tweet: {}".format(tweet_text))
+
+
+# Create a function that calls the TweetOut function every minute
+counter = 0
+
+# Infinitely loop
+while(True):
+
+    # Call the TweetQuotes function and specify the tweet number
+    TweetOut(counter)
+
+    # Once tweeted, wait 60 seconds before doing anything else
+    time.sleep(10)
+
+    # Add 1 to the counter prior to re-running the loop
+    counter = counter + 1
